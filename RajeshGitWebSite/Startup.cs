b7b698b7c229aa44a2c1a8ad983bfe654f9cc780
rajesh_patil74 <rajesh_patil74@hotmail.com>
2014-05-08 14:46:17 +0530

    First Commit

---
+++ RajeshGitWebSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RajeshGitWebSite.Startup))]
+namespace RajeshGitWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
